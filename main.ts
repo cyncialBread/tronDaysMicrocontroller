ffe5b240a87aea9fa40820a32285f49ab5cdc86f
cyncialBread <31447440+cyncialBread@users.noreply.github.com>
2020-09-25 10:12:09 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,16 @@ input.buttonA.onEvent(ButtonEvent.Click, function () {
         pause(200)
         light.setPixelColor(index, 0x00ff00)
     }
+    for (let index = 0; index < 2; index++) {
+        light.showRing(
+        `black black black black black black black black black black`
+        )
+        pause(100)
+        light.showRing(
+        `green green green green green green green green green green`
+        )
+        pause(100)
+    }
     makerController.player1.setAnalogThreshold(ArcadeAnalogButton.LeftRight, -500, 500)
     while (true) {
         makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
