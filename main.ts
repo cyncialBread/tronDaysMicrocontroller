770c1add0f767023f2ea86062310e845a283d16a
Vanhaar <31447440+Vanhaar@users.noreply.github.com>
2020-09-25 09:52:45 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.buttonA.onEvent(ButtonEvent.Click, function () {
+    begin = true
+    pause(200)
+    for (let index = 0; index <= 9; index++) {
+        pause(200)
+        light.setPixelColor(index, 0x00ff00)
+    }
+    makerController.player1.setAnalogThreshold(ArcadeAnalogButton.LeftRight, -500, 500)
+    while (true) {
+        makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
+        control.waitMicros(50)
+    }
+})
+let begin = false
+while (!(begin)) {
+    light.showRing(
+    `white white white white white white white white white white`
+    )
+    pause(100)
+    light.showRing(
+    `black black black black black black black black black black`
+    )
+    pause(100)
+}
